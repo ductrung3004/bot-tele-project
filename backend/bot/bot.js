82d577d46388a7aa1bbcd0e117eb47ecd9cb29bc
ductrung3004 <ductrungnguyen3004@gmail.com>
2023-10-01 16:45:12 +0700

    added helpCommand

--- backend/bot/bot.js
+++ backend/bot/bot.js
@@ -1,12 +1,28 @@
 const botStart = () => {
   const { Telegraf } = require('telegraf');
   const bot = new Telegraf(process.env.BOT_TOKEN);
-  bot.command('start', ctx => {
-      console.log(ctx.from)
-      bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Supporting chat bot.\nPlease use /help to get the commands.', {
-      })
+  const startCommand = () =>{
+    bot.command('start',(ctx) => {
+        // console.log(ctx.from)
+        // bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Supporting chat bot.\nPlease use /help to get the commands.', {
+        ctx.reply('Hello there! Welcome to the Supporting chat bot.\nPlease use /help to get the commands.');
+        })
+    return;
+  }
+  const helpCommand = () =>{
+    bot.command('help', (ctx) => {
+      ctx.reply( 
+  `Control the bot by using these commands:
+  1. /start : Hello there! Use /help to get the commands.
+  2. /help : show the commands
+  3. /check : check if the bot is listening`);
+    return;
     })
+  }
+  startCommand();
+  helpCommand();
   bot.launch();
   return;
+
 }
 module.exports = { botStart };
--- backend/index.js
+++ backend/index.js
@@ -15,19 +15,6 @@ expressApp.get("/", (req, res) => {
 });
 
 
-
-// bot.command('ethereum', ctx => {
-//   var rate;
-//   console.log(ctx.from)
-//   axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
-//   .then(response => {
-//     console.log(response.data)
-//     rate = response.data.ethereum
-//     const message = `Hello, today the ethereum price is ${rate.usd}USD`
-//     bot.telegram.sendMessage(ctx.chat.id, message, {
-//     })
-//   })
-// })
 const serverStart = () => {
   botFile.botStart();
 
